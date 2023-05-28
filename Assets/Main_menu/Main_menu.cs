dfa78fbc8c3cd679473f6837481b6cba475f90b9
Sebastien <sebastien.pichot@epitech.eu>
2023-05-28 14:01:53 +0200

    [ADD] main menu

---
+++ Assets/Main_menu/Main_menu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
